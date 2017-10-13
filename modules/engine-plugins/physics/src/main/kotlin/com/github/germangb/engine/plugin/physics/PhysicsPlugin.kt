d1a3d4ca116acded6064fac5856290794e17c417
germän gômez <germangb42@gmail.com>
2017-10-14 00:03:43 +0200

    remove dummy physics

--- modules/engine-plugins/physics/src/main/kotlin/com/github/germangb/engine/plugin/physics/PhysicsPlugin.kt
+++ modules/engine-plugins/physics/src/main/kotlin/com/github/germangb/engine/plugin/physics/PhysicsPlugin.kt
@@ -10,17 +10,3 @@ interface PhysicsPlugin : Plugin, PhysicsDevice
  * Get physics plugin
  */
 val Context.physics get() = getPlugin(PhysicsPlugin::class) as? PhysicsDevice
-
-/**
- * Dummy physics
- */
-object DummyPhysicsPlugin : PhysicsPlugin {
-    override fun createWorld(gravity: Vector3c) = TODO()
-    override fun onPreInit() = Unit
-    override fun onPostInit()  = Unit
-    override fun onPreUpdate() = Unit
-    override fun onPostUpdate() = Unit
-    override fun onPreDestroy() = Unit
-    override fun onPostDestroy() = Unit
-}
-
