a95f2a03956e51f60c2935fa7a9679ab975ff120
germän gômez <germangb42@gmail.com>
2017-10-14 00:04:07 +0200

    remove import

--- modules/engine-plugins/physics/src/main/kotlin/com/github/germangb/engine/plugin/physics/PhysicsPlugin.kt
+++ modules/engine-plugins/physics/src/main/kotlin/com/github/germangb/engine/plugin/physics/PhysicsPlugin.kt
@@ -2,7 +2,6 @@ package com.github.germangb.engine.plugin.physics
 
 import com.github.germangb.engine.core.Context
 import com.github.germangb.engine.core.Plugin
-import com.github.germangb.engine.math.Vector3c
 
 interface PhysicsPlugin : Plugin, PhysicsDevice
 
