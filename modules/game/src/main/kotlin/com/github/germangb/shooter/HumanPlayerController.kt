ca6648f45b05b94406159fd67b36cf932cab9f33
germän gômez <germangb42@gmail.com>
2017-11-14 23:22:44 +0100

    kinematic demo

--- modules/game/src/main/kotlin/com/github/germangb/shooter/HumanPlayerController.kt
+++
@@ -1,8 +0,0 @@
-package com.github.germangb.shooter
-
-import com.github.germangb.engine.input.InputDevice
-
-/** Controller controlled by input */
-class HumanPlayerController(val input: InputDevice) : PlayerController {
-    //TODO
-}
--- modules/game/src/main/kotlin/com/github/germangb/shooter/PlayerController.kt
+++
@@ -1,6 +0,0 @@
-package com.github.germangb.shooter
-
-/** Interface for the game to interact with the player */
-interface PlayerController {
-    //TODO
-}
